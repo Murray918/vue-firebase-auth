eb26b1f111936f01e6a31bf59a54bdf7ebca67f0
Andrew Murray <darbyguitars@gmail.com>
2020-01-17 14:51:36 -0600

    create the login component hook the login component up to the router
    	modified:   src/router/index.js
    	new file:   src/views/Login.vue

---
+++ src/views/Login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h3>Sign In</h3>
+    <input type="text" placeholder="Email" />
+    <input type="password" placeholder="Password" />
+    <button>Connection</button>
+    <p>you dont have an account ? you can create one</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login",
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style scoped></style>
