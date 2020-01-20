46a9985f0c6dfdbb409d5996dd22457866ed8fd3
Andrew Murray <darbyguitars@gmail.com>
2020-01-20 12:00:15 -0600

    create the login view and component import the login with vue router and lazy load it remove logo from home view and add to the login add scoped styling for all the login
    	modified:   src/App.vue
    	modified:   src/router/index.js
    	modified:   src/views/Home.vue
    	modified:   src/views/Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="login">
     <h3>Sign In</h3>
     <input type="text" placeholder="Email" />
     <input type="password" placeholder="Password" />
@@ -18,4 +18,35 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.login {
+  display: flex;
+  width: 100%;
+  height: 100%;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+input {
+  margin: 10px 0;
+  width: 40%;
+  padding: 15px;
+}
+
+button {
+  margin-top: 10px;
+  width: 15%;
+  cursor: pointer;
+}
+
+p {
+  margin-top: 40px;
+  font-size: 13px;
+}
+
+p a {
+  text-decoration: underline;
+  cursor: pointer;
+}
+</style>
